2ecf6d197aa5ceb6a9715f7f9f950585fd297015
Connor Dillon <connor@codewithconnor.com>
2019-07-05 14:18:33 -0700

    fixed naming errors in main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 
-const navItem = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 //Set Initial State of Menu (closed)
 let showMenu = false;
@@ -18,7 +18,7 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        NavItems.forEach(item => item.classList.add('show'));
+        navItems.forEach(item => item.classList.add('show'));
 
         //Set Menu State
         showMenu = true;
@@ -28,48 +28,9 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        NavItems.forEach(item => item.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
 
         //Set Menu State
-        showMenu = true;
-    }
-};
-
-// ! =========================
-
-// Select DOM Items
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
-
-const navItem = document.querySelectorAll('.nav-items');
-
-//Set Initial State of Menu (closed)
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    if (!showMenu) {
-        //
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
-        NavItems.forEach(item => item.classList.add('show'));
-
-        //Set Menu State
-        showMenu = true;
-    } else {
-        //
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        NavItems.forEach(item => item.classList.remove('show'));
-
-        //Set Menu State
-        showMenu = true;
+        showMenu = false;
     }
 };
