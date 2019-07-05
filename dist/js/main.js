f695e6bf7e61d1bc1849742c84dbe4cbdd47e9bc
Connor Dillon <connor@codewithconnor.com>
2019-07-05 14:14:15 -0700

    temp commit to fix main.js, test env

--- dist/js/main.js
+++ dist/js/main.js
@@ -34,3 +34,42 @@ function toggleMenu() {
         showMenu = true;
     }
 };
+
+// ! =========================
+
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItem = document.querySelectorAll('.nav-items');
+
+//Set Initial State of Menu (closed)
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        //
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        NavItems.forEach(item => item.classList.add('show'));
+
+        //Set Menu State
+        showMenu = true;
+    } else {
+        //
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        NavItems.forEach(item => item.classList.remove('show'));
+
+        //Set Menu State
+        showMenu = true;
+    }
+};
